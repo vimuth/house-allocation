29efbcce6f9410f8f4cbc6c129b6f4f6c30b68b7
Vimuth Fernando <Vimuth@Vimuths-MacBook-Pro.local>
2014-01-24 11:04:49 +0530

    created view events page

--- js/custom.js
+++ js/custom.js
@@ -1,37 +1,16 @@
 showEvents = function() {
-
-	 $.ajax({
-	 type : "GET",
-	 url : "http://10.100.4.54:9763/TestApp2/controller/event.jag",
-	 success : function(msg) {
-	 $('#main').replaceWith(msg);
-	 }
-	 });
-	var data = {"events":[ {
-		"id" : "51",
-		"name" : "boxtest",
-		"description" : "test"
-	}, {
-		"id" : "52",
-		"name" : "boxtest",
-		"description" : "test"
-	}, {
-		"id" : "53",
-		"name" : "boxtest",
-		"description" : "test"
-	}, {
-		"id" : "54",
-		"name" : "boxtest",
-		"description" : "test"
-	}, {
-		"id" : "55",
-		"name" : "boxtest",
-		"description" : "test"
-	} ]};
-
-	$.get('templates/event.html', function(template) {
-		var html = Mustache.to_html(template, data);
-		$("#main").html(html);
+	
+	$.ajax({
+		type : "GET",
+		headers: {"Accept": "application/json"},
+		url : "http://10.100.4.54:9763/TestApp2/controller/event.jag",
+		success : function(msg) {
+			$.get('templates/event.html', function(template) {
+				var html = Mustache.to_html(template,
+						$.parseJSON(msg).eventCollection);
+				$("#main").html(html);
+			});
+		}
 	});
 
 };
