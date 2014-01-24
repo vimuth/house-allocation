376e7af63c708eacaf2d9dc67c5740347c2ae240
Vimuth Fernando <Vimuth@Vimuths-MacBook-Pro.local>
2014-01-24 10:32:28 +0530

    created sample get events page

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,37 @@
-showEvents = function(){
-	var template = $.get('templates/event.html');
-	$('#main').replaceWith(Mustache.to_html(template, [{"id":"51","name":"boxtest","description":"test"},{"id":"52","name":"boxtest","description":"test"},{"id":"53","name":"boxtest","description":"test"},{"id":"54","name":"boxtest","description":"test"},{"id":"55","name":"boxtest","description":"test"}]));
+showEvents = function() {
+
+	 $.ajax({
+	 type : "GET",
+	 url : "http://10.100.4.54:9763/TestApp2/controller/event.jag",
+	 success : function(msg) {
+	 $('#main').replaceWith(msg);
+	 }
+	 });
+	var data = {"events":[ {
+		"id" : "51",
+		"name" : "boxtest",
+		"description" : "test"
+	}, {
+		"id" : "52",
+		"name" : "boxtest",
+		"description" : "test"
+	}, {
+		"id" : "53",
+		"name" : "boxtest",
+		"description" : "test"
+	}, {
+		"id" : "54",
+		"name" : "boxtest",
+		"description" : "test"
+	}, {
+		"id" : "55",
+		"name" : "boxtest",
+		"description" : "test"
+	} ]};
+
+	$.get('templates/event.html', function(template) {
+		var html = Mustache.to_html(template, data);
+		$("#main").html(html);
+	});
+
 };
