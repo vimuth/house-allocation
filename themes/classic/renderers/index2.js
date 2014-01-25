cc8c30013f5aca34ee08862abac26d3bb6b8733c
Vimuth <Vimuth@gmail.com>
2014-01-25 12:39:08 +0530

    enabling caramel

---
+++ themes/classic/renderers/index2.js
@@ -0,0 +1,12 @@
+var render = function(theme, data, meta, require) {
+	theme('simple', {
+		title : [ {
+			partial : 'title',
+			context : data.title
+		} ],
+		body : [ {
+			partial : 'body',
+			context : data.body
+		} ]
+	});
+};
