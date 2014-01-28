151294cd4ca8426c58d840d6c85d57012ebb9287
Vimuth <Vimuth@gmail.com>
2014-01-28 16:26:38 +0530

    edit-event at progess

--- themes/classic/renderers/edit-event.js
+++ themes/classic/renderers/edit-event.js
@@ -9,7 +9,7 @@ var render = function(theme, data, meta, require) {
 			context : data.title
 		}],
 		main : [ {
-			partial : 'event-list',
+			partial : 'edit-event',
 			context : data.body
 		} ]
 	});
