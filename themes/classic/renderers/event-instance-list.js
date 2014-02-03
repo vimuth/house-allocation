9f8a936cbcd69fc94d13f81f066eadc32ee2fdf8
Vimuth <Vimuth@gmail.com>
2014-02-03 11:47:28 +0530

    Implemeted view current events and view history

---
+++ themes/classic/renderers/event-instance-list.js
@@ -0,0 +1,16 @@
+var render = function(theme, data, meta, require) {
+	theme('simple', {
+		header : [ {
+			partial : 'header',
+			context : data.header
+		} ],
+		title :[{
+			partial : 'title',
+			context : data.title
+		}],
+		main : [ {
+			partial : 'event-instance-list',
+			context : data.main
+		} ]
+	});
+};
