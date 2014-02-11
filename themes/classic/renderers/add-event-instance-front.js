38f49a7d157961a98dd67edaae677f3b8a5b4aa7
sdkottegoda <sdkottegoda@gmail.com>
2014-02-11 05:53:06 +0530

    renamed add event instance files

---
+++ themes/classic/renderers/add-event-instance-front.js
@@ -0,0 +1,16 @@
+var render = function(theme, data, meta, require) {
+	theme('simple', {
+		header : [ {
+			partial : 'header',
+			context : data.header
+		} ],
+		title :[{
+			partial : 'title',
+			context : data.title
+		}],
+		main : [ {
+			partial : 'add-event-instance-front',
+			context : data.events
+		} ]
+	});
+};
