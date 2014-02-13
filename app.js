af543a8ea7a5de300b950cf4ae7e5b7b7a9676ab
Vimuth <Vimuth@gmail.com>
2014-02-13 10:07:34 +0530

    fixed edit page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var caramel = require('caramel');
 
 caramel.configs({
-    context: '/TestApp2',
+    context: '/HouseManagement',
     negotiation: true,
     themer: function () {
         return 'classic';
