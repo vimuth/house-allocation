912c802015c72a0a442cf0dbffe58649adaef56f
sdkottegoda <sdkottegoda@gmail.com>
2014-02-13 07:29:16 +0530

    added events by type page

---
+++ themes/classic/renderers/events-by-type.js
@@ -0,0 +1,20 @@
+var render = function(theme, data, meta, require) {
+	theme('simple', {
+		header : [ {
+			partial : 'header',
+			context : data.header
+		} ],
+		title :[{
+			partial : 'title',
+			context : data.title
+		}],
+		sidenav : [ {
+			partial : 'sidenav',
+			context : data.sidenav
+		} ],
+		main : [ {
+			partial : 'all-events-list',
+			context : data.eventInstances
+		} ]
+	});
+};
